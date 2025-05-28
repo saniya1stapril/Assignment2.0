7d89feba0c97b92f5255238118065eb1429e40f0
saniya koyal <dodoprogramming019@gmail.com>
2025-05-28 19:01:12 +0530

    Update 2task2.py

--- 2task2.py
+++ 2task2.py
@@ -1,4 +1,9 @@
-num=0
+a=0
 for i in range(1,51):
-    num+=i
-print("The Sum of integars from 1 to 50 is:",num)
+    a+=i
+print("The Sum of integars from 1 to 50 is:",a)
+
+OUTPUT
+PS C:\Users\SANIYA\OneDrive\Desktop\dodothecoder> & "C:/Program Files/Python313/python.exe" c:/Users/SANIYA/OneDrive/Desktop/dodothecoder/output/2task2.py
+The Sum of integars from 1 to 50 is: 1275
+
