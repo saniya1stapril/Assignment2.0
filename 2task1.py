5c8e18872c213f455ac9de765558aea6b71f75c0
saniya koyal <dodoprogramming019@gmail.com>
2025-05-28 18:59:46 +0530

    Update 2task1.py

--- 2task1.py
+++ 2task1.py
@@ -3,3 +3,11 @@ if a%2==0:
     print("This is an even number")
 else:
     print("this is an odd number")
+
+OUTPUT
+PS C:\Users\SANIYA\OneDrive\Desktop\dodothecoder> & "C:/Program Files/Python313/python.exe" c:/Users/SANIYA/OneDrive/Desktop/dodothecoder/2task1.py
+Enter a Number: 4
+This is an even number
+PS C:\Users\SANIYA\OneDrive\Desktop\dodothecoder> & "C:/Program Files/Python313/python.exe" c:/Users/SANIYA/OneDrive/Desktop/dodothecoder/2task1.py
+Enter a Number: 7
+this is an odd number
